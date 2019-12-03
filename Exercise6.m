054a0295c4f1ad187a824e3a18f69c6e9b37f118
eliecerecology <eliecerecology@gmail.com>
2019-12-03 22:04:58 +0200

    right plot on exercise 6 1.3

--- Exercise6.m
+++ Exercise6.m
@@ -9,8 +9,8 @@ end
 %% A system in frequency domain
 % An example system:
 % y[n] = (x[n] + x[n-1] + x[n-2])/3
-aa = [1] %ones(1,3)/3; xs
-bb = [1.0005, - 0.0005] %[1, 0, 0]; % zeros because 2nd order system and there is no y[n-1] and y[n-2] terms
+bb = ones(1,2)*0.97; %ones(1,3)/3; xs
+aa = [1, - 0.95]; %[1, 0, 0]; % zeros because 2nd order system and there is no y[n-1] and y[n-2] terms
 
 % Zeros and poles of H(z)
 zz = roots(bb) % zeros of the system
@@ -22,4 +22,4 @@ poly([0 0])   % you can also generate a polynomial if you know the roots
 zplane(zz,pp) % an another way to call the zplane (by giving the roots)
 bb
 zz
-freqz(bb,aa)          % frequency response, look documentation for freqz for more info
+freqz(bb,aa, 2000, 22050)          % frequency response, look documentation for freqz for more info
